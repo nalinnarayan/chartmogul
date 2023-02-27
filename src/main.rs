a0096a3430d416b4cc8e2e89dd8bd243fb428ea7
Nalin Narayan <dev@nalinnarayan.com>
2023-02-27 17:14:14 +0530

    Updated the API to accept dynamic paramters from the UI Form

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,10 @@ use dotenv::dotenv;
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct MRR{
 	date: String,
-  mrr: f64,
+    mrr: f64,
 }
 
-//Structure for sending MRR retrieval request to ChartMogul
+//Structure for receiving MRR retrieval request from UI Form
 #[derive(FromForm, Debug, Clone, Serialize, Deserialize)]
 pub struct ToRetrieveMRR{
   start_date: String,
@@ -27,51 +27,44 @@ pub struct ToRetrieveMRR{
   interval: String,
 }
 
+//Structure for handling MRR data received from ChartMogul
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct FromRetrieveMRR{
   entries: Vec<MRR>,
 }
 
 
-
-//#[post("/getmrr", data = "<mrrparams>")]
-#[get("/getmrr")]
-//fn get_mrr(mrrparams: Form<ToRetrieveMRR>) -> Result<Json<Vec<MRR>>,Error>{
-fn get_mrr() -> Result<Json<Vec<MRR>>,Error>{
+//this route receives the MRR form data from the UI, calls the 
+//ChartMogul metrics API's retrieve-MRR endpoint, and returns MRR 
+//with date.
+#[post("/getmrr", data = "<mrrparams>")]
+fn get_mrr(mrrparams: Form<ToRetrieveMRR>) -> Result<Json<Vec<MRR>>,Error>{
 	dotenv().ok();
+  //Getting the Auth headers ready
   let req_client = reqwest::blocking::Client::new();
   let chartmogul_base_api_url = env::var("CHARTMOGUL_BASE_API_URL").unwrap();
   let chartmogul_api_key = env::var("CHARTMOGUL_API_KEY").unwrap();
   let chartmogul_authorization_header = format!("{}{}", "Basic ", chartmogul_api_key);
   let chartmogul_authorization_header_str = chartmogul_authorization_header.as_str();
+  //Inserting all headers
   let mut headers = HeaderMap::new();
-  headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+    headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
 	headers.insert(ACCEPT, HeaderValue::from_static("application/json"));
 	headers.insert(AUTHORIZATION, HeaderValue::from_str(chartmogul_authorization_header_str).unwrap());
-  /*let mrr_params = ToRetrieveMRR{
-    start_date: mrrparams.start_date.clone(),
-    end_date: mrrparams.end_date.clone(),
-    interval: mrrparams.interval.clone(),
-  };*/
+  
+  //Getting the request url and query parameters ready
   let to_chartmogul_retrieve_mrr_url = format!("{}{}", chartmogul_base_api_url.as_str(), "/metrics/mrr?");
   let parameters = &[
-			("start-date", "2019-01-01"),
-			("end-date", "2019-04-30"),
-      ("interval", "month"),
-		];
-  /*let parameters = &[
 			("start-date", mrrparams.start_date.as_str()),
 			("end-date", mrrparams.end_date.as_str()),
-      ("interval", mrrparams.interval.as_str()),
-		];*/
-	let url = to_chartmogul_retrieve_mrr_url + &serde_urlencoded::to_string(parameters).unwrap();
+      		("interval", mrrparams.interval.as_str()),
+		];
+  let url = to_chartmogul_retrieve_mrr_url + &serde_urlencoded::to_string(parameters).unwrap();
+  //Making the call to ChartMogul Metrics API's Retrieve MRR endpoint
   let res = req_client.get(url.as_str())
       .headers(headers)
       .send()?;
-  /*let res = req_client.post(to_chartmogul_retrieve_mrr_url.as_str())
-			.headers(headers)
-			.json(&mrr_params)
-			.send()?;*/
+  
   let deserialized_res: FromRetrieveMRR = res.json().unwrap();
   Ok(Json(deserialized_res.entries))
 
